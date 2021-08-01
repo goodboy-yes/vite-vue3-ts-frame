e01129145277de78968abfcecec49008d48ca1f2
goodboy-yes <gkckx37@163.com>
2021-08-01 15:43:02 +0800

    chore(deps): 集成commitlint进行提交信息格式检查

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src')
+      '@': resolve(__dirname, 'src')
     }
   },
   base: './', // 设置打包路径
